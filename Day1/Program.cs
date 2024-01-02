ad21cbaab8dcd15de36d91b6f800960fb4b8b2a6
servetvural <80199349+servetvural@users.noreply.github.com>
2024-01-02 11:51:21 +0000

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,37 @@
+﻿StreamReader reader = new StreamReader("input.txt");
+
+int total = 0;
+
+while (true) {
+    string theLine = reader.ReadLine();
+    if (theLine == null)
+        break;
+    else
+    {
+        string str = "";
+        for (int i=0; i<theLine.Length; i++)
+        {
+            if (Char.IsDigit(theLine[i]))
+            {
+                str = theLine[i].ToString();
+                break;
+            }                
+        }
+        for (int i = theLine.Length - 1; i >= 0; i--)
+        {
+            if (Char.IsDigit(theLine[i]))
+            {
+                str += theLine[i].ToString();
+                break;
+            }
+        }
+        if (str != "")
+        {
+            total += int.Parse(str);
+        }
+    }
+}
+
+Console.WriteLine(total);
+
+Console.ReadLine();
