ebf677ba801a5e739213ed92c11cfd71ea124579
servetvural <80199349+servetvural@users.noreply.github.com>
2024-01-02 15:03:08 +0000

    SOLUTION 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,6 +1,56 @@
-﻿StreamReader reader = new StreamReader("input.txt");
+﻿//SOLUTION 1
+//StreamReader reader = new StreamReader("input.txt");
+
+//int total = 0;
+
+//while (true)
+//{
+//    string theLine = reader.ReadLine();
+//    if (theLine == null)
+//        break;
+//    else
+//    {
+//        string str = "";
+//        for (int i = 0; i < theLine.Length; i++)
+//        {
+//            if (Char.IsDigit(theLine[i]))
+//            {
+//                str = theLine[i].ToString();
+//                break;
+//            }
+//        }
+//        for (int i = theLine.Length - 1; i >= 0; i--)
+//        {
+//            if (Char.IsDigit(theLine[i]))
+//            {
+//                str += theLine[i].ToString();
+//                break;
+//            }
+//        }
+//        if (str != "")
+//        {
+//            total += int.Parse(str);
+//        }
+//    }
+//}
+
+
+//SOLUTION 2
+StreamReader reader = new StreamReader("input.txt");
 
 int total = 0;
+
+List<Tuple<string, int>> nums = new List<Tuple<string, int>>() {
+    Tuple.Create("one",1), 
+    Tuple.Create("two",2),
+    Tuple.Create("three",3),
+    Tuple.Create("four",4),
+    Tuple.Create("five",5),
+    Tuple.Create("six",6),
+    Tuple.Create("seven",7),
+    Tuple.Create("eight",8),
+    Tuple.Create("nine",9)
+};
 
 while (true) {
     string theLine = reader.ReadLine();
@@ -8,12 +58,17 @@ while (true) {
         break;
     else
     {
+        int firstindex = 0;
+        string firstdigit = "";
+        int lastindex = 0;
+        string lastdigit = "";
         string str = "";
         for (int i=0; i<theLine.Length; i++)
         {
             if (Char.IsDigit(theLine[i]))
             {
-                str = theLine[i].ToString();
+                firstdigit = theLine[i].ToString();
+                firstindex = i;
                 break;
             }                
         }
@@ -21,10 +76,28 @@ while (true) {
         {
             if (Char.IsDigit(theLine[i]))
             {
-                str += theLine[i].ToString();
+                lastdigit = theLine[i].ToString();
+                lastindex = i;
                 break;
             }
         }
+
+        for (int i=0; i < nums.Count; i++)
+        {
+            if (theLine.IndexOf(nums[i].Item1) >=0 && theLine.IndexOf(nums[i].Item1) < firstindex)
+            {
+                firstindex = theLine.IndexOf(nums[i].Item1);
+                firstdigit = nums[i].Item2.ToString();
+            }
+            if ( theLine.LastIndexOf(nums[i].Item1) > lastindex)
+            {
+                lastindex = theLine.LastIndexOf(nums[i].Item1);
+                lastdigit = nums[i].Item2.ToString();
+            }
+        }
+       
+        str = firstdigit + lastdigit;
+                
         if (str != "")
         {
             total += int.Parse(str);
